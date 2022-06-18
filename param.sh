137b575b8736222a5b74e44efc5353c0c0725f65
gck0 <20210112+gck0@users.noreply.github.com>
2022-06-18 14:34:56 +0200

    Create param.sh

---
+++ param.sh
@@ -0,0 +1,18 @@
+  GNU nano 6.2                                                                               param.sh                                                                                         
+#!/bin/bash
+
+##default values##
+DATATOPIC=dt0
+RESPONSETOPIC=rt0
+
+if [[ -z "$1" || -z "$2" ]]; then
+  echo PARAMETERS: "<datatopic> <responsetopic>"
+  echo setting to defaults
+else
+  DATATOPIC=$1
+  RESPONSETOPIC=$2
+fi
+echo DataTopic = \"${DATATOPIC}\"
+echo ResponseTopic = \"${RESPONSETOPIC}\"
+cp ./cmd/res/configuration-base.toml ./cmd/res/configuration.toml
+sed -i "s/\"DataTopic\"/\"$DATATOPIC\"/;s/\"ResponseTopic\"/\"$RESPONSETOPIC\"/" ./cmd/res/configuration.toml
