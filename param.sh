465cb81ddb83517b49f230db891467fc7e57ddbf
gck0 <20210112+gck0@users.noreply.github.com>
2022-06-18 14:17:45 +0200

    Create param.sh

---
+++ param.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+##default values##
+DATATOPIC=dt0
+RESPONSETOPIC=rt0
+
+
+Help()
+{
+  echo PARAMETERS: "<datatopic> <responsetopic>"
+}
+
+Check()
+{
+  if [[ -z "$1" || -z "$2" ]]; then
+    Help
+    exit 1
+  fi
+}
+
+Exec()
+{
+  echo exec
+  DATATOPIC=$1
+  RESPONSETOPIC=$2
+  cp ./cmd/res/configuration-base.toml ./cmd/res/configuration.toml
+  sed -i 's/\"DataTopic\"/\"$DATATOPIC\"/;s/\"ResponseTopic\"/\"$RESPONSETOPIC\"/' ./cmq/res/configuration.toml
+}
+
+
+## MAIN ##
+Check
+Exec
