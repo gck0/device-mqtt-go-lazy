d8a813147831e347ef2177d7b30e982496a8d1d2
gck0 <20210112+gck0@users.noreply.github.com>
2022-06-18 16:05:26 +0200

    Update param.sh

--- param.sh
+++ param.sh
@@ -1,4 +1,5 @@
 ##default values##
+CLIENTID=mqtt-dv0
 DATATOPIC=dt0
 RESPONSETOPIC=rt0
 
@@ -12,4 +13,4 @@ fi
 echo DataTopic = \"${DATATOPIC}\"
 echo ResponseTopic = \"${RESPONSETOPIC}\"
 cp ./cmd/res/configuration-base.toml ./cmd/res/configuration.toml
-sed -i "s/\"DataTopic\"/\"$DATATOPIC\"/;s/\"ResponseTopic\"/\"$RESPONSETOPIC\"/" ./cmd/res/configuration.toml
+sed -i "s/\"DataTopic\"/\"$DATATOPIC\"/;s/\"ResponseTopic\"/\"$RESPONSETOPIC\"/;s/\"clientid-mqtt\"/\"$CLIENTID\"/" ./cmd/res/configuration.toml
